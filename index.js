1d62447ddd2c030520f31356758e908e1bcf12cf
VanshAggarwal4664 <er.vanshaggarwal@gmail.com>
2024-10-06 10:49:24 +0530

    add pattern printing code

--- index.js
+++ index.js
@@ -7,13 +7,30 @@ function findEven(nums){
     }
   }
 }
-findEven(nums);
+
 
 function findOdd(nums){
     for(let i=0;i<nums.length;i++){
       if(nums[i]%2!=0){
-          console.log("this is even number",nums[i])
+          console.log("this is odd number",nums[i])
       }
     }
   }
-findOdd(nums)
+
+
+let n=5;
+function patternPrint(n){
+    for(let i=0;i<n;i++){
+        let bag="";
+        for(let j=0;j<n;j++){
+            bag+="*"+" ";
+        }
+        
+        console.log(bag);
+    }
+
+}
+
+findEven(nums);
+findOdd(nums);
+patternPrint(n);
